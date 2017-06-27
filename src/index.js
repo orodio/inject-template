2832750803980385084a31e01d2aabc8c68c8df2
James Hunter <james@bjorkman.ca>
2017-06-27 16:18:27 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "babel-polyfill"
 import { interleave } from '@orodio/interleave'
 
+// isFn :: any -> Boolean
 const isFn = fn =>
   typeof fn === 'function'
 
+// inject :: <[String], ...[String | Function]> -> Object -> String
 export const inject = (str, ...augs) => (data={}) => {
   return interleave(str, augs)
     .map(node => isFn(node) ? node(data) : String(node))
